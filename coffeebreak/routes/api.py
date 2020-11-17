595ac7d7486a71a58331fcd72a06cace09985408
Tina <me@tina.moe>
2020-11-17 19:38:56 +0100

    Add preset system with first preset (osu)

--- coffeebreak/routes/api.py
+++ coffeebreak/routes/api.py
@@ -1,26 +1,65 @@
 import io
 import json
 import os
+from datetime import datetime
 
 from flask import abort, current_app, send_file, request, url_for, jsonify
 from coffeebreak import app, image, params, root_path, storage
 
+def fill_presets(data):
+    with open(root_path / 'presets.json') as f:
+        presets = json.load(f).get(data['settings']['preset'])
+    if not presets:
+        print('Could not find preset for {}'.format(preset_name))
+        return data
+    presets['class'] = presets.get('class', {})
+    for key, value in presets.get('presets', {}).items():
+        if key == 'level':
+            actual_level = data.get('level')
+            if not actual_level:
+                continue
+            actual_level = float(actual_level)
+            levels = list(value.keys())
+            levels.sort(reverse=True)
+            for level in levels:
+                if actual_level > float(level):
+                    presets[key] = value[level]
+                    break
+            continue
+        if key == 'judge':
+            presets['class'][key] = data.get(key, value)
+            continue
+        presets['class'][key] = value.get(data.get(key))
+    del presets['presets']
+    for key, value in presets.items():
+        if isinstance(value, dict):
+            for k2, v2 in value.items():
+                data[key] = data.get(key, {})
+                data[key][k2] = data[key].get(k2, v2)
+        else:
+            data[key] = data.get(key, value)
+    return data
+
 def handle_settings(data):
     if 'settings' not in data:
         return data
+    if data['settings'].get('preset'):
+        data = fill_presets(data)
     if data['settings'].get('auto_combo'):
         data['max_chart_combo'] = sum(data.get('judges', {}).values())
+    if data['settings'].get('auto_date'):
+        data['date'] = data.get('date') or '{} UTC'.format(datetime.utcnow().replace(microsecond=0))
     return data
 
 def filter_data(data=None):
     data = data or dict(request.values)
-    return {k: v for k, v in data.items() if k in params.ARGUMENTS}
+    return {k: v for k, v in data.items() if k in params.ARGUMENTS and v != ''}
 
 @app.route('/api/card.html', methods=['GET', 'POST'])
 def generate_html(data=None):
     data = data or filter_data(data)
     for k in params.OBJECTS:
-        if isinstance(data[k], str):
+        if isinstance(data.get(k), str):
             try:
                 data[k] = json.loads(data[k])
             except json.JSONDecodeError:
